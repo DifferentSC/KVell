501d369e9b7ba688026da5e0033f6862e9ad946a
DifferentSC <gyewonlee@snu.ac.kr>
2020-08-16 10:10:47 +0900

    Add static to get_slab_context

--- slabworker.h
+++ slabworker.h
@@ -28,7 +28,9 @@ uint64_t get_rdt(struct slab_context *ctx);
 void set_rdt(struct slab_context *ctx, uint64_t val);
 int get_worker(struct slab *s);
 int get_nb_disks(void);
-struct slab_context *get_slab_context(void *item);
 struct slab *get_item_slab(int worker_id, void *item);
 size_t get_item_size(char *item);
+
+static struct slab_context *get_slab_context(void *item);
+
 #endif
