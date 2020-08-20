425d7723372d7b388e20091eda13d08c089fafcb
DifferentSC <gyewonlee@snu.ac.kr>
2020-08-20 19:11:49 +0900

    Reduce sleep time

--- slabworker.c
+++ slabworker.c
@@ -388,7 +388,7 @@ static void *worker_slab_init(void *pdata) {
       volatile size_t pending = ctx->sent_callbacks - ctx->processed_callbacks;
       while(!pending && !io_pending(ctx->io_ctx)) {
          if(!PINNING) {
-            usleep(2);
+            usleep(1);
          } else {
             NOP10();
          }
